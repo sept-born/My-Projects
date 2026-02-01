9c0eaa6656363ce1f311a00ea1149c4abef3e9cf
Tanish Agrawal <agarwaltanish31@gmail.com>
2026-02-01 22:11:10 +0530

    backend setup completer

---
+++ Backend/src/db/index.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    );
+
+    console.log(
+      `\n MONGO_DB connection successfull ${connectionInstance.connection.host}`
+    );
+  } catch (error) {
+    console.log("MONGO_DB connection failed !!!", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
