38861090dfc0326e14dc367589f9284266cf1aa5
Tanish Agrawal <agarwaltanish31@gmail.com>
2026-02-07 00:44:15 +0530

    Recipe page fixed

--- Fronted/Nutritrack-fronted/src/pages/Signup.jsx
+++ Fronted/Nutritrack-fronted/src/pages/Signup.jsx
@@ -1,28 +1,69 @@
+import { useState } from "react";
+import axios from "axios";
 export default function Signup() {
+  const [formData, setformData] = useState({
+    fullname: "",
+    email: "",
+    username: "",
+    password: "",
+  });
+  const [error, setError] = useState("");
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setformData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+  const handleSubmit = async () => {
+    await axios
+      .post("http://localhost:8000/api/v1/users/register", formData)
+      .catch(setError(error));
+  };
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-100">
-      <div className="bg-white p-8 rounded shadow w-96">
-        <h2 className="text-2xl font-bold mb-6 text-center">Create Account</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="bg-white p-8 rounded shadow w-96">
+          {/* ERROR MESSAGE */}
+          {error && (
+            <p className="mb-4 text-red-500 text-sm text-center">{error}</p>
+          )}
 
-        <input
-          className="w-full mb-3 p-2 border rounded"
-          placeholder="Full Name"
-        />
-        <input className="w-full mb-3 p-2 border rounded" placeholder="Email" />
-        <input
-          className="w-full mb-3 p-2 border rounded"
-          placeholder="Username"
-        />
-        <input
-          className="w-full mb-4 p-2 border rounded"
-          placeholder="Password"
-          type="password"
-        />
+          <h2 className="text-2xl font-bold mb-6 text-center">
+            Create Account
+          </h2>
 
-        <button className="w-full bg-green-600 text-white py-2 rounded">
-          Sign Up
-        </button>
-      </div>
+          <input
+            className="w-full mb-3 p-2 border rounded"
+            placeholder="Full Name"
+            name="fullname"
+            onChange={handleChange}
+          />
+          <input
+            className="w-full mb-3 p-2 border rounded"
+            placeholder="Email"
+            name="email"
+            onChange={handleChange}
+          />
+          <input
+            className="w-full mb-3 p-2 border rounded"
+            placeholder="Username"
+            name="username"
+            onChange={handleChange}
+          />
+          <input
+            className="w-full mb-4 p-2 border rounded"
+            placeholder="Password"
+            type="password"
+            onChange={handleChange}
+            name="password"
+          />
+
+          <button className="w-full bg-green-600 text-white py-2 rounded">
+            Sign Up
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
